d4913c9ed2c133998da68c73c28dceafd6a35fb2
15634519721 <2535019174@qq.com>
2021-07-01 10:13:22 +0800

    20210701

--- src/chop1/Dog.java
+++ src/chop1/Dog.java
@@ -1,5 +1,13 @@
 package chop1;
 
 public class Dog {
+    string name="无名氏";          //姓名
+    int health=100;               //健康值
+    int love=0;                   //亲密度
+    string strain="拉布拉多犬";     //品种
+    //输出
+    public void show(){
+        System.out.println("姓名是：");
 
+    }
 }
