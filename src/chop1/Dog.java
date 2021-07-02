5f2fdd707022c957a0250c63d8a3340c11360fda
张炜林 <2535019174@qq.com>
2021-07-02 08:28:31 +0800

    20210702

--- src/chop1/Dog.java
+++ src/chop1/Dog.java
@@ -5,9 +5,10 @@ public class Dog {
     int health=100;               //健康值
     int love=0;                   //亲密度
     String strain="拉布拉多犬";     //品种
-    //无参构造方法
+    //无参构造方法:方法名与类名相同，无返回值
+    //先执行无参构造方法，再执行类的方法
     public Dog(){
-        
+        System.out.println("执行无参的构造方法");
     }
     //输出
     public void show(){
---
+++ src/chop1/Test1.java
@@ -0,0 +1,15 @@
+package chop1;
+
+/**
+ * main方法
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+    //测试方法
+    public static void test1(){
+        Dog dog=new Dog();
+
+    }
+}
