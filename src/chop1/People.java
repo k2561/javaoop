d0c745cfd2e1a99a090269ddbc50542532a888e9
张炜林 <2535019174@qq.com>
2021-07-02 11:40:56 +0800

    20210702

---
+++ src/chop1/People.java
@@ -0,0 +1,21 @@
+package chop1;
+
+public class People {
+    private String name;
+    private String  sex;
+    private String phone;
+    private int age;
+    private double weight;
+
+    public People() {
+
+    }
+
+    public People(String name, String sex, String phone, int age, double weight) {
+        this.name = name;
+        this.sex = sex;
+        this.phone = phone;
+        this.age = age;
+        this.weight = weight;
+    }
+}
--- src/chop1/Test2.java
+++ src/chop1/Test2.java
@@ -1,10 +1,15 @@
 package chop1;
-
+//封装调用
 public class Test2 {
     public static void main(String[] args) {
-        test1();
+        testStudnt();
     }
-    public static void  test1(){
-
+    public static void  testStudnt(){
+        //有参构造方法
+        Student student=new Student(1,"王五",18,"山东济南");
+        //无参构造方法
+        Student student1=new Student();
+        student1.setName("李四");
+        System.out.println("姓名是："+student1.getName());
     }
 }
