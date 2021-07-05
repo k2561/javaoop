c240ced80bac926477fb43ab19853fa6b11917b8
张炜林 <2535019174@qq.com>
2021-07-05 08:45:42 +0800

    20210705

--- src/chop2/index.java
+++ src/chop2/index.java
@@ -1,5 +1,38 @@
 package chop2;
-
+//封装
 public class index {
+    private int id;
+    private String name;
 
+    public index(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public index() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    //重载
+    public void show(){
+        System.out.println("1");
+    }
+    public int show(int a){
+        System.out.println("2");
+        return a;
+    }
 }
