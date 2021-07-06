a2017e7c385a4dbf4314f0c260efa3d48cb4914c
张炜林 <2535019174@qq.com>
2021-07-06 09:07:48 +0800

    20210706

---
+++ src/chop2/proj/Cat.java
@@ -0,0 +1,12 @@
+package chop2.proj;
+
+/**
+ * 猫
+ */
+public class Cat extends Pet{
+    //抽象方法
+    @Override
+    public void print() {
+
+    }
+}
--- src/chop2/proj/Dog.java
+++ src/chop2/proj/Dog.java
@@ -1,6 +1,19 @@
 package chop2.proj;
 //狗：继承于宠物
-public class Dog extends Pet{
+//常量方法
+public final class Dog extends Pet{
+    /**
+     * final的用法
+     * final修饰类的时候，该类不能被继承
+     * final修饰属性的时候，该属性为常量
+     *      只能在声明的时候给它值，不能修改。
+     *      常量名均大写
+     * final修饰方法的时候，该方法不能被重写
+     * final Dog dog=new Dog("欧欧")
+     * dog.name="毛毛"      //可以
+     * dog=new Dog("默默")  //不可以
+     * 使用final修饰引用型变量，变量的值是固定不变的，而变量所指向的对象的属性值是可变的
+     */
     String strain;
 
     public String getStrain() {
