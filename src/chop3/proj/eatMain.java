82f479b90fd02cca018eb099fc16a777eed90406
张炜林 <2535019174@qq.com>
2021-07-06 10:33:31 +0800

    20210706

--- src/chop3/proj/eatMain.java
+++ src/chop3/proj/eatMain.java
@@ -5,6 +5,12 @@ package chop3.proj;
  */
 public class eatMain {
     public static void main(String[] args) {
-
+        //对象
+        Master master=new Master("小王",10000);
+        Dog dog=new Dog("旺旺",100,100,"哈士奇");
+        Cat cat=new Cat("豆豆",100,100,"黄色");
+        //方法
+        master.feed(dog);
+        master.feed(cat);
     }
 }
