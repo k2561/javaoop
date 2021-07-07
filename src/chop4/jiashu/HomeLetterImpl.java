4630f81345faeed179557920a7f81952dcd46be3
zhang-weilinwww <2535019174@qq.com>
2021-07-07 11:33:17 +0800

    20210707

--- src/chop4/jiashu/HomeLetterImpl.java
+++ src/chop4/jiashu/HomeLetterImpl.java
@@ -6,39 +6,24 @@ package chop4.jiashu;
  */
 public class HomeLetterImpl implements HomeLetter {
 
-    /* (non-Javadoc)
-     * @see cn.jbit.letter.HomeLetter#writeBody()
-     */
     public void writeBody() {
         System.out.println("    我在这里挺好的。\n" +
                 "    我会努力学习的，已经学到Java OOP啦！\n"+
                 "    您二老保重身体啊！\n");
     }
 
-    /* (non-Javadoc)
-     * @see cn.jbit.letter.HomeLetter#writeGreeting()
-     */
     public void writeGreeting() {
         System.out.println("    此致\n敬礼");
     }
 
-    /* (non-Javadoc)
-     * @see cn.jbit.letter.HomeLetter#writeHello()
-     */
     public void writeHello() {
         System.out.println("    你们好吗？\n");
     }
 
-    /* (non-Javadoc)
-     * @see cn.jbit.letter.HomeLetter#writeSelf()
-     */
     public void writeSelf() {
         System.out.println("\t\t周杰 \n\t2010年6月1日");
     }
 
-    /* (non-Javadoc)
-     * @see cn.jbit.letter.HomeLetter#writeTitle()
-     */
     public void writeTitle() {
         System.out.println("亲爱的爸爸、妈妈：");
     }
