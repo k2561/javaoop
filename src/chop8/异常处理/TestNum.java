e1c740584a713b780f979cb3373248d359fc619a
zhang-weilinwww <2535019174@qq.com>
2021-07-08 09:51:34 +0800

    20210708

--- src/chop8/异常处理/TestNum.java
+++ src/chop8/异常处理/TestNum.java
@@ -13,6 +13,7 @@ public class TestNum {
     static Scanner scanner=new Scanner(System.in);
     public static void test1(){
         try {
+            //可能出现异常的代码
             System.out.println("请输入被除数");
             //scanner.hasNextInt() 判断
             int num1=scanner.nextInt();
@@ -21,9 +22,14 @@ public class TestNum {
             int shang=num1/num2;
             System.out.println(num1+"/"+num2+"="+shang);
         }catch(Exception ex){
+            //出现异常之后所执行的代码
+            System.out.println("异常错误位置:"+ex.getStackTrace()[0]);
+            System.out.println("异常信息:"+ex.getMessage());
+            ex.printStackTrace();
             System.out.println("除数不能为零或者不是数字");
         }
         finally {
+            //不管出现还是没有出现异常，该代码块都执行
             System.out.println("执行完成");
         }
     }
