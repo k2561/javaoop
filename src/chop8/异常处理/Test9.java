8424c1fe7b468679bc322b706fe38581287e64aa
zhang-weilinwww <2535019174@qq.com>
2021-07-08 11:24:03 +0800

    不处理Checked异常

---
+++ src/chop8/异常处理/Test9.java
@@ -0,0 +1,15 @@
+package chop8.异常处理;
+
+import java.io.*;
+/**
+ * 不处理Checked异常。
+ */
+public class Test9 {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+        // 创建指定文件的流。
+        fis = new FileInputStream(new File("accp.txt"));
+        // 创建指定文件的流。
+        fis.close();
+    }
+}
