9ad030b040dc2d853bab28900104bf708caf43c1
zhang-weilinwww <2535019174@qq.com>
2021-07-08 10:37:51 +0800

    20210708

--- src/chop8/异常处理/TestNum.java
+++ src/chop8/异常处理/TestNum.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class TestNum {
     public static void main(String[] args) {
-        test1();
+        test2();
     }
     //计算商
     static Scanner scanner=new Scanner(System.in);
@@ -30,6 +30,37 @@ public class TestNum {
         }
         finally {
             //不管出现还是没有出现异常，该代码块都执行
+            System.out.println("执行完成");
+        }
+    }
+    //异常类型
+
+    /**
+     * 常用的异常类型：
+     * 1.Exception：所有异常的父类
+     * 	2.ArithmeticException:当除数是0的时候出现的异常
+     * 	3.ArryOutIndexOfBoundsException:数组下标越界的异常
+     * 	4.NullPointerException:空指针异常。
+     * 	5.ClassNotFoundException:找不到类的异常。
+     * 	6.InputMismatchexception：欲得到数据类型与实际的数据类型不匹配，比如说用int类型接收了一个字符串类型的值。
+     * 	Exception 	异常层次结构的根类
+     * ArithmeticException	算术错误情形，如以零作除数
+     * ArrayIndexOutOfBoundsException	数组下标越界
+     * NullPointerException	尝试访问 null 对象成员
+     * ClassNotFoundException	不能加载所需的类
+     * InputMismatchException	欲得到数据类型与实际输入类型不匹配
+     * IllegalArgumentException	方法接收到非法参数
+     * ClassCastException	对象强制类型转换出错
+     * NumberFormatException	数字格式转换异常，如把"abc"转换成数字
+     */
+    public static void test2(){
+        try {
+            int[]nums=new int[3];
+            nums[3]=0;
+            System.out.println(nums[3]);
+        }catch (ArrayIndexOutOfBoundsException ae){
+            System.out.println("数组越界");
+        }finally {
             System.out.println("执行完成");
         }
     }
