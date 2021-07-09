93c301bfbef1cd18413b9cb277944e4dd7d132cd
zhang-weilinwww <2535019174@qq.com>
2021-07-09 09:42:23 +0800

    20210709

--- src/chop8/异常处理/TestLog4j.java
+++ src/chop8/异常处理/TestLog4j.java
@@ -7,18 +7,27 @@ import java.util.Scanner;
  * 平均分
  */
 public class TestLog4j {
-    private static Logger logger=Logger.getLogger(TestLog4j.class.getName());
+    private final static Logger logger=Logger.getLogger(TestLog4j.class.getName());
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int []nums=new int[3];
-        int sum=0;
-        for (int i=0;i<nums.length;i++){
-            logger.debug("请输入第"+(i+1)+"位学生的成绩");
-            System.out.println("请输入第"+(i+1)+"位学生的成绩");
-            nums[i]=scanner.nextInt();
-            sum+=nums[i];
+        try {
+            Scanner scanner=new Scanner(System.in);
+            int []nums=new int[3];
+            int sum=0;
+            for (int i=0;i<nums.length;i++){
+                logger.debug("请输入第"+(i+1)+"位学生的成绩");
+                System.out.println("请输入第"+(i+1)+"位学生的成绩");
+                nums[i]=scanner.nextInt();
+                sum+=nums[i];
+            }
+            System.out.println("总分是:"+sum);
+            logger.debug("输出的平均分是:"+sum/nums.length);
+            System.out.println("平均分是:"+sum/nums.length);
+        }catch (Exception ex){
+            logger.error("出现异常啦！");
+            logger.error("异常信息是:"+ex.getMessage());
+        }finally {
+            System.out.println("程序结束");
+            logger.debug("程序结束");
         }
-        System.out.println("总分是:"+sum);
-        System.out.println("平均分是:"+sum/nums.length);
     }
 }
