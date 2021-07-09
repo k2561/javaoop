bb6432befc32aa88053bb0ad8c8615bbd610d114
zhang-weilinwww <2535019174@qq.com>
2021-07-09 09:33:26 +0800

    日志文件

--- src/chop8/异常处理/TestLog4j.java
+++ src/chop8/异常处理/TestLog4j.java
@@ -1,16 +1,19 @@
 package chop8.异常处理;
 
-import java.util.Scanner;
+import org.apache.log4j.Logger;
 
+import java.util.Scanner;
 /**
  * 平均分
  */
 public class TestLog4j {
+    private static Logger logger=Logger.getLogger(TestLog4j.class.getName());
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int []nums=new int[3];
         int sum=0;
         for (int i=0;i<nums.length;i++){
+            logger.debug("请输入第"+(i+1)+"位学生的成绩");
             System.out.println("请输入第"+(i+1)+"位学生的成绩");
             nums[i]=scanner.nextInt();
             sum+=nums[i];
