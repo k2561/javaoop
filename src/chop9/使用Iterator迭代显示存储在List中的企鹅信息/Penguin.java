94d6f5a1701975ed065cfc1a5ebb1c1d6ca40163
zhang-weilinwww <2535019174@qq.com>
2021-07-11 19:25:59 +0800

    20210711

---
+++ src/chop9/使用Iterator迭代显示存储在List中的企鹅信息/Penguin.java
@@ -0,0 +1,37 @@
+package chop9.使用Iterator迭代显示存储在List中的企鹅信息;
+
+/**
+ * 宠物企鹅。
+ */
+public class Penguin {
+    String name = "无名氏"; // 昵称
+    int health = 100; // 健康值
+    int love = 0; // 亲密度
+    String sex = "Q仔"; // 性别
+
+    /**
+     * 两个参数构造方法。
+     */
+    public Penguin(String name, String sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+
+}
