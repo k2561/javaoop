ebbb31417032ff9e7d2ef29e7b63ca36a600cec6
zhang-weilinwww <2535019174@qq.com>
2021-07-16 19:22:57 +0800

    20210714

--- src/chop5/index.java
+++ src/chop5/index.java
@@ -1,7 +1,20 @@
 package chop5;
 
 /**
- *
+ *使用static修饰属性和代码块
+ * static属性
+ * 属于这个类所有
+ * 通过“对象名.static属性名” 、“类名.static属性名” 访问
+ * static方法
+ * 通过“类名.static方法名”和“对象名.static方法名” 访问
+ * static代码块
+ * 主要作用是实现static属性的初始化
+ * 当类被载入的时候执行它，且只被执行一次
+ * 面向对象设计的思想
+ * 使用类图理解类的关系
+ * 类的封装
+ * 构造方法的使用
+ * this、static关键字的使用
  */
 public class index {
 
