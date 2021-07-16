33b94f961c3244eafa7bdf28242c3ad3dd0ed55b
zhang-weilinwww <2535019174@qq.com>
2021-07-16 20:01:27 +0800

    20210717

--- src/chop7/index.java
+++ src/chop7/index.java
@@ -94,6 +94,119 @@ package chop7;
  * 强调
  * 聚合、组合是关联的特殊形式，从语义上区分
  * 总体来说，关系强弱程度上依赖<关联<聚合<组合
+ *
+ *
+ *
+ *
+ *
+ * 掌握面向对象设计原则之开闭原则
+ * 面向对象设计原则
+ * 在使用面向对象思想进行系统设计时，总结出了若干面向对象设计原则，分别是
+ * 单一职责原则
+ * 开闭原则
+ * 里氏代换原则
+ * 依赖注入原则
+ * 接口分离原则
+ * 迪米特原则
+ * 组合/聚合复用原则
+ * 这些原则是面向对象设计的基石，坚持这些原则是提升面向对象设计质量的重要保障
+ *
+ *
+ * 开闭原则定义：
+ * OCP：Open for extention, Closed for modification Principle，即对扩展开放，对修改关闭原则
+ *
+ * 该原则的思想是：可以通过扩展来满足变化，而不需要修改代码，或者说在设计一个模块的时候，应当使这个模块可以在不被修改的前提下被扩展
+ *需求发生变化，按照工资算法二计算工资，如何实现呢
+ *class Salary { // 工资类
+ * 	public void computerSalary() {
+ * 		System.out.println("采用工资算法一计算工资");
+ *        }
+ * }
+ * class Accountant { // 会计类
+ * 	public void ComputerSalary(Salary salary) {
+ * 		salary.computerSalary();
+ *    }
+ * }
+ * class TestOCP { // 测试类
+ * 	public static void main(String[] args) {
+ * 		Accountant accountant = new Accountant ();
+ * 		 accountant.ComputerSalary(new Salary());
+ *    }
+ * }
+ *
+ *
+ * 通过修改Salary类的computerSalary() 来实现，不利于程序的稳定
+ *class Salary { // 工资类
+ * 	public void computerSalary() {
+ * 		System.out.println(“采用工资算法二计算工资");
+ *        }
+ * }
+ * class Accountant { //会计类
+ * 	public void ComputerSalary(Salary salary) {
+ * 		salary.computerSalary();
+ *    }
+ * }
+ * class TestOCP { // 测试类
+ * 	public static void main(String[] args) {
+ * 		 Accountant accountant = new Accountant ();
+ * 		 accountant.ComputerSalary(new Salary());
+ *    }
+ * }
+ *创建工资接口，并提供实现类
+ *interface Salary { // 工资接口
+ * 	public void computerSalary();
+ * }
+ * class BeijingSalary implements Salary { //北京工资实现类
+ * 	public void computerSalary() {
+ * 		System.out.println("采用工资算法一计算工资");	}
+ * }
+ * class Accountant { //会计类
+ * 	public void ComputerSalary(Salary salary) {
+ * 		salary.computerSalary();	}
+ * }
+ * class TestOCP { // 测试类
+ * 	public static void main(String[] args) {
+ * 		 Accountant accountant = new Accountant ();
+ * 		 accountant.ComputerSalary(new BeijingSalary());	}
+ * }
+ *
+ *增加Salary接口的新实现，而不是修改原有类
+ *interface Salary { // 工资接口
+ * 	public void computerSalary();
+ * }
+ * class HebeiSalary implements Salary { //河北工资实现类
+ * 	public void computerSalary() {
+ * 		System.out.println(“采用工资算法二计算工资");	}
+ * }
+ * class Accountant { // 会计类
+ * 	public void ComputerSalary(Salary salary) {
+ * 		salary.computerSalary();	}
+ * }
+ * class TestOCP { // 测试类
+ * 	public static void main(String[] args) {
+ * 		 Accountant accountant = new Accountant ();
+ * 		 accountant.ComputerSalary(new HebeiSalary());	}
+ * }
+ *实现 ：
+ * 实现开闭原则的关键是抽象
+ * 定义一个抽象层，只规定功能而不提供实现，实现通过定义具体类来完成
+ * 当需求变化时，不是通过修改抽象层来完成，而是通过定义抽象层的新实现完成，即通过扩展来完成
+ *
+ * 通过抽象类及接口，规定了具体类的特征作为抽象层，相对稳定，不需修改，从而满足“对修改关闭”
+ * 从抽象类导出的具体类可以改变系统的行为，从而满足“对扩展开放”
+ *好处 ：
+ * 通过扩展已有软件系统，可提供新的行为，以满足对软件的新需求，提高了软件系统的适应性和灵活性
+ *
+ * 已有的软件模块，特别是最重要的抽象层模块不能再修改，提高了软件系统的一定的稳定性和延续性
+ *
+ * 这样的设计同时也满足了可复用性与可维护性;
+ * 总结：
+ * 实现开闭原则的关键是抽象
+ * 抽象层相对稳定，不需修改，需求变化后通过重新定义抽象层的新实现来完成
+ *
+ * 即使无法百分之百的做到开闭原则，但朝这个方向努力，可以显著改善一个系统的结构
+ * 对系统每个部分都肆意地进行抽象也不是一个好主意，应该仅仅对程序中需求频繁变化部分进行抽象
+ * 拒绝不成熟的抽象和抽象本身一样重要
  */
 public class index {
 
