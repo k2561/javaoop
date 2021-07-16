c80dec76f2a74487ccdc7ef5ed601b65d2a57edf
zhang-weilinwww <2535019174@qq.com>
2021-07-16 19:13:52 +0800

    20210714

---
+++ src/chop5/quickhit/Test.java
@@ -0,0 +1,11 @@
+package chop5.quickhit;
+
+/**
+ * 玩家玩游戏。
+ */
+public class Test {
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.play();
+    }
+}
